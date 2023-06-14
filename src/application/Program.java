301fdbd81cb2660cb8b69fd890aae13ba1591d05
Jean Castilho <jeandersoncastilho@hotmail.com>
2023-06-14 19:14:23 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Salve Maloka");
+		Position p = new Position(3, 5);
+		System.out.println(p);
+		
 	}
 
 }
