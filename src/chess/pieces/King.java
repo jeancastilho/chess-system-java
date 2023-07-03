78bacaeeb13fe7ccb77ab048662349f8f5992953
Jean Castilho <jeandersoncastilho@hotmail.com>
2023-07-03 20:38:34 -0300

    Implementing possible moves of king

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -16,10 +17,61 @@ public class King extends ChessPiece {
 	public String toString() {
 		return "K";
 	}
+	
+	//METODO AUXILIAR PARA SABER SE A PEÇA "REI" PODE MOVER PARA ALGUM LUGAR
+	private boolean canMove(Position position) {
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		return p == null || p.getColor() != getColor();
+	}
 
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		
+		Position p = new Position(0,0);
+		
+		//VERIFICA AS POSIÇÕES DISPONIVEIS ACIMA
+		p.setValues(position.getRow() - 1, position.getColumn());
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// VERIFICA AS POSIÇÕES DISPONIVEIS ESQUERDA
+		p.setValues(position.getRow(), position.getColumn() - 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// VERIFICA AS POSIÇÕES DISPONIVEIS DIREITA
+		p.setValues(position.getRow(), position.getColumn() + 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// VERIFICA AS POSIÇÕES DISPONIVEIS NO NOROESTE (DIAGONAL SUPERIOR ESQUEDA )
+		p.setValues(position.getRow() - 1, position.getColumn() - 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// VERIFICA AS POSIÇÕES DISPONIVEIS NO NORDESTE (DIAGONAL SUPERIOR DIREITA )
+		p.setValues(position.getRow() - 1, position.getColumn() + 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// VERIFICA AS POSIÇÕES DISPONIVEIS NO SUDOESTE (DIAGONAL INFERIOR ESQUERDA )
+		p.setValues(position.getRow() + 1, position.getColumn() - 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// VERIFICA AS POSIÇÕES DISPONIVEIS NO SUDESTE (DIAGONAL INFERIOR DIREITA )
+		p.setValues(position.getRow() + 1, position.getColumn() + 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
 		return mat;
 	}
 
